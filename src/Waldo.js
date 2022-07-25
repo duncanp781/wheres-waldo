d27fa75c47af50e8fa384e4a7b48c1fdfd3d8af1
Duncan Peckham <hazard.dp@gmail.com>
2022-07-25 12:03:19 -0400

    Add Image Selection

--- src/Waldo.js
+++ src/Waldo.js
@@ -1,28 +1,50 @@
 import React, {Component } from "react";
-import waldo from "./waldo.jpg";
+import waldo from "./waldo-hard.jpeg";
 import "./waldo-style.css";
 
 class Waldo extends Component{
   constructor(props){
     super(props);
     this.imgRef = React.createRef()
+    this.state = {
+      clickCoords: null,
+      circle: null,
+    }
   }
 
   handleClick = (e) => {
-    console.log('mouse coords', e.clientX, e.clientY);
-    let coords = this.imgRef.current.getClientRects()[0]
-    console.log('image', coords.x, coords.y);
-    console.log('Relative Coords: ', e.clientX - coords.x, e.clientY - coords.y)
+    let coords = this.getRelCoords(e)
+    this.setState({
+      clickCoords: coords,
+    });
   }
+
+
+  getRelCoords = (e) => {
+    let imgInfo = this.imgRef.current.getClientRects()[0]
+    let [relX, relY] = [e.clientX - imgInfo.x, e.clientY - imgInfo.y];
+    let [w,h] = [imgInfo.width, imgInfo.height];
+    return [relX/w, relY/h];
+  }
+
+  reltoAbs(rel){
+    console.log(rel);
+    let [relX, relY] = rel;
+    let imgInfo = this.imgRef.current.getClientRects()[0];
+    let [w,h] = [imgInfo.width, imgInfo.height];
+    return [relX*w + imgInfo.x, relY*h + imgInfo.y]
+  }
+
   render() {
+    let absCoords = this.state.clickCoords && this.reltoAbs(this.state.clickCoords);
+    let circle = absCoords && {
+      x: absCoords[0],
+      y: absCoords[1],
+      r: 6
+    }
     return (
       <div className="waldo">
-        <div>
-          sPace
-        </div>
-        <div>
-          More SPace
-        </div>
+        {this.state.clickCoords && <Circle circle = {circle}/>}
         <img
           onClick={this.handleClick}
           src={waldo}
@@ -33,5 +55,25 @@ class Waldo extends Component{
     );
   }
 };
+
+const Circle = ({circle}) => {
+  let {x,y,r} = circle;
+  return (
+    
+    <div className = 'circle' style = {{
+      borderRadius: '100%',
+      border: '3px solid black',
+      position: 'absolute',
+      width: `${r}vw`,
+      height: `${r}vw`,
+      transform : 'translateY(-50%) translateX(-50%)',
+      top: `${y}px`,
+      left: `${x}px`,
+    }}>
+
+    </div>
+  )
+
+}
 
 export default Waldo;
